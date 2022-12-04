947bd65549b0120948ee1470b4393b7b01b9e1bd
IvanRigs <110514271+IvanRigs@users.noreply.github.com>
2022-12-03 23:17:28 -0600

    Delete Minaliv directory

--- Minaliv/src/minaliv/Main.java
+++
@@ -1,7 +0,0 @@
-package minaliv;
-
-public class Main {
-    public static void main(String[] args){
-        new Interfaces(); //Ejecuta la clase "Interfaces", que a su vez ejecuta las interfaces (El main solo llevara esto)
-    }
-}   
